e820b0522346e6917e44709fa6a3e060db44ef9a
Martinius B. Tomter <martinius.tomter@me.com>
2019-09-09 18:10:04 +0200

    Initial commit

---
+++ src/test/java/httpClient.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class httpClient {
+
+    @Test
+    void mathShouldWork() {
+        assertEquals(5, 3+2);
+    }
+
+    @Test
+    void moreMathShouldWork() {
+        assertEquals(10, 6+4);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("urlecho.appspot.com", 80);
+        socket.getOutputStream().write("GET /echo?status=200&Content-Type=text%2Fhtml&body=Hello%20world! HTTP/1.1\r\n".getBytes());
+        socket.getOutputStream().write("Host: urlecho.appspot.com\r\n".getBytes());
+        socket.getOutputStream().write("\r\n".getBytes());
+        socket.getOutputStream().flush();
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
+    }
+}
